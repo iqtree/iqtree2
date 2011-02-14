ffd657f1461cc1893e4e538c211717c8bf28f741
Bui Quang Minh <minh.bui@univie.ac.at>
2011-02-14 22:58:44 +0000

    dynamic programming for kmeans

--- src/ratemeyerdiscrete.cpp
+++ src/ratemeyerdiscrete.cpp
@@ -22,6 +22,103 @@
 #include "ratemeyerdiscrete.h"
 #include "kmeans/KMeans.h"
 
+
+/**
+	Solve k-means problem for one-dimensional data with dynamic programming
+	@param n number of data points
+	@param k number of clusters
+	@param data data point of size n: x[0..n-1]
+	@param center (OUT) output k centers of k clusters: center[0...k-1] will be filled
+	@param cluster (OUT) cluster assignments for each data point: cluster[0...n-1] will be filled
+	@return the minimum sum of squares over all k clusters
+*/
+double kMeansOneDim(int n, int k, double *data, double *center, int *cluster) {
+	int i, j, m;
+	/**
+		dynamic programming cost matrix, c[i][j] = cost of i clusters for {x1...xj}
+	*/
+	double *c[k]; 
+	/**
+		id is used to trace back the minimal solution
+	*/
+	double *id[k]; 
+	/** 
+		c1[i][j] = cost of 1 cluster for {xi...xj}
+	*/
+	double *c1[n];
+	/** 
+		m1[i][j] = mean of {xi...xj}
+	*/
+	double *m1[n];
+	
+	double x[n]; // sorted data points
+
+	// allocate memory 
+	for (i = 0; i < k; i++) c[i] = new double[n];
+	for (i = 0; i < k; i++) id[i] = new double[n];
+	for (i = 0; i < n; i++) c1[i] = new double[n];
+	for (i = 0; i < n; i++) m1[i] = new double[n];
+
+	// first sort data into x
+	memmove(x, data, sizeof(double)*n);
+	std::sort(x, x+n);
+	// first compute c1 matrix
+	for (i = 0; i < n; i++) {
+		double sum = 0.0;
+		for (j = i; j < n; j++) {
+			sum += x[j];
+			double mean = sum / (j-i+1);
+			m1[i][j] = mean;
+			double ss = 0; 
+			for (m = i; m <= j; m++) 
+				ss += (x[m]-mean)*(x[m]-mean); // sum of squared difference
+				//ss += fabs(x[m]-mean); // sum of absolute difference
+			c1[i][j] = ss;
+		}
+	}
+
+	/* now compute dynamic programming matrix */
+	// initialize the 1st row
+	for (j = 0; j < n; j++) {
+		c[0][j] = c1[0][j];
+		id[0][j] = -1;
+	}
+	for (i = 1; i < k; i++) {
+		// no i clusters exist for less than i data points
+		for (j = 0; j < i; j++) { c[i][j] = INFINITY; id[i][j] = -1; }
+		for (j = i; j < n; j++) {
+			c[i][j] = INFINITY;
+			for (m = i-1; m < j; m++)
+				if (c[i][j] > c[i-1][m] + c1[m+1][j]) {
+					c[i][j] = c[i-1][m] + c1[m+1][j];
+					id[i][j] = m;
+				}
+		}
+	}
+
+	double min_cost = c[k-1][n-1];
+	int bound[k+1];
+	// now trace back
+	bound[k] = n-1;
+	for (i = k-1; i >= 0; i--) {
+		bound[i] = id[i][bound[i+1]];
+	}
+
+	for (i = 0; i < k; i++) {
+		center[i] = m1[bound[i]+1][bound[i+1]];
+		for (j = 0; j < n; j++)
+			if (data[j] <= x[bound[i+1]] && data[j] >= x[bound[i]+1])
+				cluster[j] = i;
+	}
+
+	// free memory
+	for (i = n-1; i >= 0; i--) delete [] m1[i];
+	for (i = n-1; i >= 0; i--) delete [] c1[i];
+	for (i = k-1; i >= 0; i--) delete [] id[i];
+	for (i = k-1; i >= 0; i--) delete [] c[i];
+	return min_cost;
+}
+
 RateMeyerDiscrete::RateMeyerDiscrete(int ncat)
  : RateMeyerHaeseler()
 {
@@ -49,32 +146,44 @@ void RateMeyerDiscrete::optimizeRates() {
 void RateMeyerDiscrete::classifyRatesKMeans() {
 
 	assert(ncategory > 0);
-	int nsites = phylo_tree->aln->getNSite();
+	int nptn = size();
 
 	// clustering the rates with k-means
 	//AddKMeansLogging(&cout, false);
-	double points[nsites];
-	int assignments[nsites];
-	int attempts = nsites, i;
-	for (i = 0; i < nsites; i++) {
-		if (at(phylo_tree->aln->getPatternID(i)) == MAX_SITE_RATE) 
+	double points[nptn];
+	int assignments[nptn];
+	int i;
+	for (i = 0; i < nptn; i++) {
+		if (at(i) == MAX_SITE_RATE) 
 			points[i] = log(1e6);
 		else 
-			points[i] = log(at(phylo_tree->aln->getPatternID(i)));
+			points[i] = log(at(i));
 	}
 	memset(rates, 0, sizeof(double) * ncategory);
 
-	double cost = RunKMeansPlusPlus(nsites, ncategory, 1, points, attempts, rates, assignments);
-	cout << "Rates are classified by k-means (" << attempts << " runs) with cost " << cost << endl;
+	//double cost = RunKMeansPlusPlus(nsites, ncategory, 1, points, attempts, rates, assignments);
+	double cost = kMeansOneDim(nptn, ncategory, points, rates, assignments);
+	//cout << "Rates are classified by k-means (" << attempts << " runs) with cost " << cost << endl;
+	//cout << "Minimum cost by dynamic programming is " << cost1 << endl;
+	//if (cost > cost1+1e-6)
+		//cout << "k-means++ is stuck in local minimum, global is " << cost1 << endl;
 	// assign the categorized rates
 	double sum = 0.0, ok = 0.0;
 	for (i = 0; i < ncategory; i++) rates[i] = exp(rates[i]);
-	for (i = 0; i < nsites; i++) {
-		double site_r = rates[assignments[i]];
-		if (site_r < 2*MIN_SITE_RATE) site_r = MIN_SITE_RATE;
-		if (site_r > MAX_SITE_RATE*0.99) site_r = MAX_SITE_RATE;
-		at(phylo_tree->aln->getPatternID(i)) = site_r;
-		if (site_r < MAX_SITE_RATE) { sum += site_r; ok += mean_rate; }
+	if (rates[0] < MIN_SITE_RATE) rates[0] = MIN_SITE_RATE;
+	if (rates[ncategory-1] > MAX_SITE_RATE) rates[ncategory-1] = MAX_SITE_RATE;
+	if (verbose_mode >= VB_MED) {
+		cout << "K-means cost: " << cost << endl;
+		for (i = 0; i < ncategory; i++) cout << rates[i] << " ";
+		cout << endl;
+	}
+
+	for (i = 0; i < nptn; i++) {
+		at(i) = rates[assignments[i]];
+		if (at(i) < MAX_SITE_RATE) { 
+			sum += at(i) * phylo_tree->aln->at(i).frequency; 
+			ok += mean_rate * phylo_tree->aln->at(i).frequency; 
+		}
 	}
 
 	if (fabs(sum - ok) > 1e-3) {
@@ -85,13 +194,6 @@ void RateMeyerDiscrete::classifyRatesKMeans() {
 	}
 
 	}
-	std::sort(rates, rates + ncategory);
-	if (rates[0] < MIN_SITE_RATE) rates[0] = MIN_SITE_RATE;
-	if (verbose_mode >= VB_MED) {
-		//cout << "K-means cost: " << cost << endl;
-		for (i = 0; i < ncategory; i++) cout << rates[i] << " ";
-		cout << endl;
-	}
 
 }
 
@@ -99,6 +201,7 @@ void RateMeyerDiscrete::classifyRatesKMeans() {
 double RateMeyerDiscrete::classifyRates(double tree_lh) {
 	double new_tree_lh;
 	if (ncategory > 0) {
+		cout << endl << "Classifying rates into " << ncategory << " categories..." << endl;
 		classifyRatesKMeans();
 		phylo_tree->clearAllPartialLh();
 		new_tree_lh = phylo_tree->computeLikelihood();
@@ -123,4 +226,5 @@ double RateMeyerDiscrete::classifyRates(double tree_lh) {
 	cout << endl << "Number of categories is set to " << ncategory << endl;
 	return new_tree_lh;
 }
+
 
