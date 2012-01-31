17be335d3d76f18a038fbac9f938c5c00f400424
Bui Quang Minh <minh.bui@univie.ac.at>
2012-01-31 23:08:30 +0000

    small update from home

--- src/phylosupertree.cpp
+++ src/phylosupertree.cpp
@@ -26,27 +26,31 @@ PhyloSuperTree::PhyloSuperTree()
 
 PhyloSuperTree::PhyloSuperTree(Params &params) :  PhyloTree() {
 	cout << "Reading partition model file " << params.partition_file << " ..." << endl;
-	const int MAX_SIZE = 200;
-	char buf[MAX_SIZE];
 	try {
 		ifstream in;
 		in.exceptions(ios::failbit | ios::badbit);
 		in.open(params.partition_file);
+		in.exceptions(ios::badbit);
+
+		Params origin_params = params;
+		//memcpy(&origin_params, &params, sizeof(params));
+		string part_name, model_name, aln_file, sequence_type, position_spec;
 
 		while (!in.eof()) {
-			in.getline(buf, MAX_SIZE, ',');
+			getline(in, part_name, ',');
 			if (in.eof()) break;
-			part_names.push_back(buf);
-			in.getline(buf, MAX_SIZE, ',');
-			string model_name = buf;
-			in.getline(buf, MAX_SIZE, ',');
-			string aln_file = buf;
-			in.getline(buf, MAX_SIZE, ',');
-			string seq_type = buf;
-			in.getline(buf, MAX_SIZE);
-			string position_spec = buf;
-			cout << "Reading partition " << part_names.back() << " (" << model_name << "," << 
-				aln_file << "," << seq_type << "," << position_spec << ") ..." << endl;
+			params = origin_params;
+			part_names.push_back(part_name);
+			getline(in, model_name, ',');
+			if (model_name == "*") model_name = params.model_name;
+			getline(in, aln_file, ',');
+			if (aln_file == "*") aln_file = params.aln_file;
+			getline(in, sequence_type, ',');
+			if (sequence_type=="*") sequence_type = params.sequence_type;
+			getline(in, position_spec);
+			cout << "Reading partition " << part_name << " (" << params.model_name << "," << 
+				aln_file << "," << sequence_type << "," << position_spec << ") ..." << endl;
+			Alignment *aln = new Alignment((char*)aln_file.c_str(), (char*)sequence_type.c_str(), params.intype);
 		}
 
 		in.clear();
