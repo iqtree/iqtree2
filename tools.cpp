216fb599f6f3901f5f497e0f95e22d457fd369d5
Tung Nguyen <nltung@gmail.com>
2014-01-17 19:05:21 +0100

    setback -nni5 to false

--- tools.cpp
+++ tools.cpp
@@ -644,7 +644,7 @@ void parseArg(int argc, char *argv[], Params &params) {
     params.k_representative = 4;
     params.loglh_epsilon = 0.000001;
     params.numSmoothTree = 1;
-    params.nni5 = true;
+    params.nni5 = false;
     params.leastSquareBranch = false;
     params.leastSquareNNI = false;
     params.ls_var_type = OLS;
