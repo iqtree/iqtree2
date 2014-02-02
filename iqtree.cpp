4b63c5217d5116a472c0a7fbec177e725aed5395
Tung Nguyen <nltung@gmail.com>
2014-02-03 00:57:55 +0100

    remove branch length optimization on perturbed tree again

--- iqtree.cpp
+++ iqtree.cpp
@@ -1062,7 +1062,7 @@ double IQTree::doTreeSearch() {
 					pllNewickTree *perturbTree = pllNewickParseString(perturbTreeString.str().c_str());
 					pllTreeInitTopologyNewick(pllInst, perturbTree, PLL_FALSE);
 					pllEvaluateGeneric(pllInst, pllPartitions, pllInst->start, PLL_TRUE, PLL_FALSE);
-					pllTreeEvaluate(pllInst, pllPartitions, params->numSmoothTree);
+					//pllTreeEvaluate(pllInst, pllPartitions, params->numSmoothTree);
 					pllNewickParseDestroy(&perturbTree);
 					curScore = pllInst->likelihood;
 					perturbScore = curScore;
