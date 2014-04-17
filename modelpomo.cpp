dbf2d6b7917d21f617a82b4b8a464c41c31bc51f
Dominik Schrempf <dominik.schrempf@gmail.com>
2014-04-17 16:39:40 +0200

    create pomo source files

--- modelpomo.cpp
+++ modelpomo.cpp
@@ -1,18 +1,5 @@
-/*
- * modelpomo.cpp
- *
- *  Created on: Mar 25, 2014
- *      Author: minh
- */
-
 #include "modelpomo.h"
-
-ModelPoMo::ModelPoMo() {
-	// TODO Auto-generated constructor stub
-
-}
-
-ModelPoMo::~ModelPoMo() {
-	// TODO Auto-generated destructor stub
-}
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
--- modelpomo.h
+++ modelpomo.h
@@ -1,19 +1,46 @@
-/*
- * modelpomo.h
- *
- *  Created on: Mar 25, 2014
- *      Author: minh
- */
-
-#ifndef MODELPOMO_H_
-#define MODELPOMO_H_
+#ifndef _MODELPOMO_H_
+#define _MODELPOMO_H_
 
 #include "gtrmodel.h"
 
-class ModelPoMo: public GTRModel {
-public:
-	ModelPoMo();
-	virtual ~ModelPoMo();
+class ModelPoMo : public GTRModel
+{     
+  
+public:  
+  /** 
+   * Constructor
+   * 
+   * @param tree associated tree for the model
+   * 
+   */
+  ModelPoMo(PhyloTree *tree, bool count_rate = true);
+  
+  /* TODO ModelSubst::Rates */
+
+  /* TODO ModelSubst::State_Frequ */
+
+  /* TODO */
+  virtual void init();
+
+  /* TODO */
+  virtual int getNDim();
+
+  /* TODO */
+  virtual void setVariables();
+
+  /* TODO */
+  virtual void getVariables();
+
+  /** 
+   * Destructor
+   * 
+   * 
+   * @return 
+   */
+  ~ModelPoMo();
+
+private:
+
 };
 
-#endif /* MODELPOMO_H_ */
+#endif /* _MODELPOMO_H_ */
