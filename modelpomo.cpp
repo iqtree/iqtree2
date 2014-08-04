d4baf7b9558d7fe0e2b94b38e3dd75a68bce65af
Dominik Schrempf <dominik.schrempf@gmail.com>
2014-08-04 17:52:36 +0200

    Comments.

--- modelpomo.cpp
+++ modelpomo.cpp
@@ -16,85 +16,106 @@ void ModelPoMo::init(const char *model_name, string model_params, StateFreqType 
 }
 
 ModelPoMo::~ModelPoMo() {
+	// TODO: Check with ModelPoMo::init!
 	delete [] mutation_prob;
 }
 
 
 void ModelPoMo::initMoranWithMutation() {
-	/*
-	int i=0;
 
-	for (i=0; i<num_states*(num_states-1)/2; i++) {
-		rates[i] = 1.0;
-	}
-	for (i=0; i<num_states; i++) {
-		state_freq[i] = 1.0/num_states;
-	}*/
+	// // This code was used to run a dummy JC69 model with 58 states.
+	// int i=0;
 
-	/* TODO Initialize rate matrix here. */
+	// for (i=0; i<num_states*(num_states-1)/2; i++) {
+	// 	rates[i] = 1.0;
+	// }
+	// for (i=0; i<num_states; i++) {
+	// 	state_freq[i] = 1.0/num_states;
+	// }
+
+	// TODO: Recheck this.
+	// Initialize rate matrix Q[state1,state2] = transition rate from
+	// state1 to state2.
 	int state1, state2;
 	if (verbose_mode >= VB_MED) cout << "PoMo rate matrix:" << endl;
+	// Loop over rows (transition starting from state1)
 	for (state1 = 0; state1 < num_states; state1++) {
 		double row_sum = 0.0;
+		// Loop over columns (transition to state2)
 		for (state2 = 0; state2 < num_states; state2++) {
 			if (state1 == state2) {
+				// Q = P - I
 				rate_matrix[state1*num_states+state2] = computeProb(state1, state2) - 1.0;
 			} else {
 				rate_matrix[state1*num_states+state2] = computeProb(state1, state2);
 			}
+			// Compute row sum.
 			row_sum += rate_matrix[state1*num_states+state2];
 			if (verbose_mode >= VB_MED)
 				cout << rate_matrix[state1*num_states+state2] << "\t";
 		}
 		if (verbose_mode >= VB_MED) cout << endl;
+		// Check row sum.
 		if (fabs(row_sum) > 0.000001) outError("Row sum not equal 0");
 	}
-	/* TODO Future: Initialize state freqs here. */
+
+	/* TODO: Initialize state freqs here. */
 }
 
 int ModelPoMo::getNDim() {
+	// TODO: For now, no parameters are estimated.
 	return 0;
 }
 
 double ModelPoMo::computeP(int i, int major, int minor) {
+	// Cf. Moran model with mutation.
 	int N = phylo_tree->aln->virtual_pop_size;
 	return (1.0 - mutation_prob[major*4+minor])*(i)*(N-i)/(N*N) +
 			mutation_prob[minor*4+major]*(N-i)*(N-i)/(N*N);
 }
 
 double ModelPoMo::computeR(int i, int major, int minor) {
+	// Cf. Moran model with mutation.
 	int N = phylo_tree->aln->virtual_pop_size;
 	return (mutation_prob[major*4+minor]+mutation_prob[minor*4+major])*i*(N-i)/(N*N) +
 			(1-mutation_prob[minor*4+major])*(N-i)*(N-i)/(N*N) +
 			(1-mutation_prob[major*4+minor])*i*i/(N*N);
 }
+
 void ModelPoMo::decomposeState(int state, int &i, int &nt1, int &nt2) {
 	int N = phylo_tree->aln->virtual_pop_size;
 	if (state < 4) {
+		// Fixed A, C, G or T
 		i = N;
 		nt1 = state;
 		nt2 = -1; // -1 for unknown nt
 	} else if (state < 4+(N-1)) {
+		// (iA,N-iC)
 		i = state-3;
 		nt1 = 0; // A
 		nt2 = 1; // C
 	} else if (state < 4+2*(N-1)) {
+		// (iA,N-iG)
 		i = state-3-(N-1);
 		nt1 = 0; // A
 		nt2 = 2; // G
 	} else if (state < 4+3*(N-1)) {
+		// (iA,N-iT)
 		i = state-3-2*(N-1);
 		nt1 = 0; // A
 		nt2 = 3; // T
 	} else if (state < 4+4*(N-1)) {
+		// (iC,N-iG)
 		i = state-3-3*(N-1);
 		nt1 = 1; // C
 		nt2 = 2; // G
 	} else if (state < 4+5*(N-1)) {
+		// (iC,N-iT)
 		i = state-3-4*(N-1);
 		nt1 = 1; // C
 		nt2 = 3; // T
 	} else if (state < 4+6*(N-1)) {
+		// (iG,N-iT)
 		i = state-3-5*(N-1);
 		nt1 = 2; // G
 		nt2 = 3; // T
@@ -106,9 +127,16 @@ void ModelPoMo::decomposeState(int state, int &i, int &nt1, int &nt2) {
 double ModelPoMo::computeProb(int state1, int state2) {
 	int N = phylo_tree->aln->virtual_pop_size;
 
+	// Both states are decomposed into the abundance of the first
+	// allele as well as the nucleotide of the first and the second
+	// allele.
 	int i1=0, i2=0, nt1=-1, nt2=-1, nt3=-1, nt4=-1;
 	decomposeState(state1, i1, nt1, nt2);
 	decomposeState(state2, i2, nt3, nt4);
+	// Either the first nucleotides match or the first of state 1 with
+	// the second of state 2 or the first of state 2 with the second
+	// of state 1.  Additionally, we have to consider fixed states as
+	// special cases.
 	if (nt1 == nt3 && (nt2==nt4 || nt2==-1 || nt4 == -1)) {
 		if (i1==i2) {
 			if (i1==N) {
@@ -150,6 +178,6 @@ double ModelPoMo::computeProb(int state1, int state2) {
 }
 
 double ModelPoMo::optimizeParameters(double epsilon) {
-	// TODO
+	// TODO: Optimize Parameters.
 	return 0.0;
 }
--- modelpomo.h
+++ modelpomo.h
@@ -37,6 +37,9 @@ private:
   double *mutation_prob;
 
   /**
+   * P(i,major,minor) is the probability to increase the number of
+   * major alleles from i to i+1.
+   * 
    * @param i abundance of major allele
    * @param major major allele (0: A, 1: C, 2: G, 3: T)
    * @param minor minor allele (0: A, 1: C, 2: G, 3: T)
@@ -44,6 +47,9 @@ private:
   double computeP(int i, int major, int minor);
 
   /**
+   * R(i,major,minor) is the probability of no frequency change in the
+   * Moran model with mutation at one locus with two alleles.
+   * 
    * @param i abundance of major allele
    * @param major major allele (0: A, 1: C, 2: G, 3: T)
    * @param minor minor allele (0: A, 1: C, 2: G, 3: T)
@@ -51,7 +57,8 @@ private:
   double computeR(int i, int major, int minor);
 
   /**
-   * decompose state (0..57) into abundance of two nucleotides
+   * Decompose state (0..57) into abundance of two nucleotides.
+   * 
    * @param i (OUT) abundance of nucleotide 1
    * @param nt1 (OUT) nucleotide 1 (0: A, 1: C, 2: G, 3: T)
    * @param nt2 (OUT) nucleotide 2 (0: A, 1: C, 2: G, 3: T)
@@ -59,7 +66,8 @@ private:
   void decomposeState(int state, int &i, int &nt1, int &nt2);
 
   /**
-   * compute probability of change from state1 to state2 in one Moran model generation
+   * Compute probability of change from state1 to state2 in one Moran
+   * model generation.
    */
   double computeProb(int state1, int state2);
 
