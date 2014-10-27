4135d65a89014049b8090db8577f72804f5b1b6f
minh <minh.bui@univie.ac.at>
2014-10-27 23:45:59 +0100

    truly fix Olga found bug now, removeIdenticalSeqs also work for SuperTree

--- iqtree.cpp
+++ iqtree.cpp
@@ -2742,8 +2742,9 @@ void IQTree::printIntermediateTree(int brtype) {
 }
 
 void IQTree::removeIdenticalSeqs(Params &params, StrVector &removed_seqs, StrVector &twin_seqs) {
-	if (aln->isSuperAlignment())
-		return;
+	// commented out because it also works for SuperAlignment now!
+//	if (aln->isSuperAlignment())
+//		return;
 	Alignment *new_aln;
 	if (params.root)
 		new_aln = aln->removeIdenticalSeq((string)params.root, params.gbo_replicates > 0, removed_seqs, twin_seqs);
