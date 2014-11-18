1f55ab90cf86ee8a7fc80a53f952e4788eeb9cc4
Tung Nguyen <nltung@gmail.com>
2014-11-18 10:32:27 +0100

    Add new class searchinfo

---
+++ searchinfo.cpp
@@ -0,0 +1,18 @@
+/*
+ * searchinfo.cpp
+ *
+ *  Created on: Nov 18, 2014
+ *      Author: tung
+ */
+
+#include "searchinfo.h"
+
+searchinfo::searchinfo() {
+	// TODO Auto-generated constructor stub
+
+}
+
+searchinfo::~searchinfo() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ searchinfo.h
@@ -0,0 +1,20 @@
+/*
+ * searchinfo.h
+ *
+ *  Created on: Nov 18, 2014
+ *      Author: Tung
+ */
+
+#ifndef SEARCHINFO_H_
+#define SEARCHINFO_H_
+
+class searchinfo {
+public:
+	searchinfo();
+	virtual ~searchinfo();
+private:
+	bool speedNNI;
+	bool reduction;
+};
+
+#endif /* SEARCHINFO_H_ */
