80e1aac257ca7e4b457078f2c1e56263b23117cd
Tung Nguyen <nltung@gmail.com>
2015-01-27 13:32:36 +0100

    Update compile.sh to check whether all local changes have been commited

--- test_scripts/compile.sh
+++ test_scripts/compile.sh
@@ -20,7 +20,41 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+require_clean_work_tree () {
+    # Update the index
+    git update-index -q --ignore-submodules --refresh
+    err=0
+
+    # Disallow unstaged changes in the working tree
+    if ! git diff-files --quiet --ignore-submodules --
+    then
+        echo >&2 "cannot $0: you have unstaged changes."
+        git diff-files --name-status -r --ignore-submodules -- >&2
+        err=1
+    fi
+
+    # Disallow uncommitted changes in the index
+    if ! git diff-index --cached --quiet HEAD --ignore-submodules --
+    then
+        echo >&2 "cannot $0: your index contains uncommitted changes."
+        git diff-index --cached --name-status -r --ignore-submodules HEAD -- >&2
+        err=1
+    fi
+
+    if [ $err = 1 ]
+    then
+        echo >&2 "Please commit or stash them."
+        exit 1
+    fi
+}
+
+#Check whether the git work tree is clean
+require_clean_work_tree
+
+#Fetch changes from server
 git fetch
+
+#Determine hash code of current branch
 curBranch=`git status | grep "On branch" | awk '{print $3}'`
 #Take the first 6 characters of the current head commit
 commit_cur=`git log | head -n1 | awk '{print $2}' | cut -c 1-6`
