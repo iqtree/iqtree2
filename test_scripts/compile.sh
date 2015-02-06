eecec59d297f6ca2c52da85fa7522ce1ef342b86
Tung Nguyen <nltung@gmail.com>
2015-02-06 01:37:34 +0100

    Scripts updates

--- test_scripts/compile.sh
+++ test_scripts/compile.sh
@@ -48,15 +48,15 @@ require_clean_work_tree () {
     fi
 }
 
+#Check whether the git work tree is clean
+require_clean_work_tree
+
 if [ "$#" != 1 ]
 then
   echo "Please enter the name of the local branch you want to compile"
   echo "USAGE: $0 <branch_name>" >&2
   exit 1
 fi
-
-#Check whether the git work tree is clean
-require_clean_work_tree
 
 #Fetch changes from server
 git fetch
@@ -70,29 +70,21 @@ git submodule update
 commit_cur=`git log | head -n1 | awk '{print $2}' | cut -c 1-6`
 
 #Dictionary and binary names
-head_build="build_${curBranch}"
-release_build="build_release"
-release_binary="iqtree_release"
+cur_build="build_${curBranch}"
+release_prefix="build_release"
+release_binary_prefix="iqtree_release"
 #cur_binary="iqtree_${commit_cur}"
 cur_binary="iqtree_${curBranch}"
 bin_dir="iqtree_binaries"
 
 #Clean up
-if [ -e $head_build ]
+if [ -e $cur_build ]
 then
-  rm -rf $head_build
+  rm -rf $cur_build
 fi
 if [ -e $release_build ]
 then
   rm -rf $release_build 
-fi
-if [ -e $release_binary ]
-then
-  rm -rf $release_binary
-fi
-if [ -e $cur_binary ]
-then
-  rm -rf $cur_binary
 fi
 if [ -e $bin_dir ]
 then
@@ -100,22 +92,23 @@ then
   mkdir $bin_dir
 fi
 
-mkdir $head_build 
-cmake -B${head_build} -H..
-make -C ${head_build} -j4
-cp ${head_build}/iqtree ${cur_binary} 
-#rm -rf ${head_build}
+mkdir $cur_build 
+cmake -B${cur_build} -H..
+make -C ${cur_build} -j4
+cp ${cur_build}/iqtree ${bin_dir}/${cur_binary} 
+#rm -rf ${cur_build}
 mkdir $release_build
 #Find the hash code of the most recent release in master
 commit=`git log origin/master | grep -m 1 -B 4 "release version" | grep "commit" | awk '{print $2}'`
+version=`git log origin/master | grep -m 1 -B 4 "release version" | grep "commit" | awk '{print $3}'`
 git checkout ${commit}
 git submodule update
 cmake -B${release_build} -H..
 make -C ${release_build} -j4
-cp ${release_build}/iqtree ${release_binary}
+cp ${release_build}/iqtree ${bin_dir}/${release_binary_prefix}_${version}
 git checkout ${curBranch}
 git submodule update
 
 #Clean up
-rm -rf $head_build
+rm -rf $cur_build
 rm -rf $release_build 
