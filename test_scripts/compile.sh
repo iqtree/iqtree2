0c8e4d42229001a3afeee323cfafa557fb72075d
Tung Nguyen <nltung@gmail.com>
2015-02-06 02:41:33 +0100

    Update compile.sh

--- test_scripts/compile.sh
+++ test_scripts/compile.sh
@@ -49,7 +49,7 @@ require_clean_work_tree () {
 }
 
 #Check whether the git work tree is clean
-require_clean_work_tree
+#require_clean_work_tree
 
 if [ "$#" != 1 ]
 then
@@ -58,23 +58,19 @@ then
   exit 1
 fi
 
-#Fetch changes from server
-git fetch
 
 #Determine hash code of current branch
-#curBranch=`git status | grep "On branch" | awk '{print $3}'`
-curBranch=$1
-git checkout $curBranch
-git submodule update
+#branch=`git status | grep "On branch" | awk '{print $3}'`
+branch=$1
 #Take the first 6 characters of the current head commit
 commit_cur=`git log | head -n1 | awk '{print $2}' | cut -c 1-6`
 
 #Dictionary and binary names
-cur_build="build_${curBranch}"
+cur_build="build_${branch}"
 release_build="build_release"
 release_binary_prefix="iqtree_release"
 #cur_binary="iqtree_${commit_cur}"
-cur_binary="iqtree_${curBranch}"
+cur_binary="iqtree_${branch}"
 bin_dir="iqtree_binaries"
 
 #Clean up
@@ -89,10 +85,18 @@ fi
 if [ -e $bin_dir ]
 then
   rm -rf $bin_dir
-  mkdir $bin_dir
 fi
-
+mkdir $bin_dir
 mkdir $cur_build 
+#Fetch changes from server
+git fetch
+curBranch=`git status | grep 'On branch' | awk '{print $3}'`
+if [ ${curBranch} != ${branch} ]
+then
+  git stash
+  git checkout $branch
+  git submodule update
+fi
 cmake -B${cur_build} -H..
 make -C ${cur_build} -j4
 cp ${cur_build}/iqtree ${bin_dir}/${cur_binary} 
@@ -100,13 +104,14 @@ cp ${cur_build}/iqtree ${bin_dir}/${cur_binary}
 mkdir $release_build
 #Find the hash code of the most recent release in master
 commit=`git log origin/master | grep -m 1 -B 4 "release version" | grep "commit" | awk '{print $2}'`
-version=`git log origin/master | grep -m 1 -B 4 "release version" | grep "commit" | awk '{print $3}'`
+version=`git log origin/master | grep -m 1 "release version [0-9]*" | awk '{print $3}'`
 git checkout ${commit}
 git submodule update
 cmake -B${release_build} -H..
 make -C ${release_build} -j4
 cp ${release_build}/iqtree ${bin_dir}/${release_binary_prefix}_${version}
 git checkout ${curBranch}
+git stash apply
 git submodule update
 
 #Clean up
