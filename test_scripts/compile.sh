9f5b9e795ba1cdd2846086d925f047da3088df3b
minh <minh.bui@univie.ac.at>
2015-03-05 18:01:42 +0100

    add test scripts

--- test_scripts/compile.sh
+++ test_scripts/compile.sh
@@ -49,57 +49,72 @@ require_clean_work_tree () {
 }
 
 #Check whether the git work tree is clean
-require_clean_work_tree
+#require_clean_work_tree
 
-#Fetch changes from server
-git fetch
+if [ "$#" != 1 ]
+then
+  echo "Please enter the name of the local branch you want to compile"
+  echo "USAGE: $0 <branch_name>" >&2
+  exit 1
+fi
+
 
 #Determine hash code of current branch
-curBranch=`git status | grep "On branch" | awk '{print $3}'`
+#branch=`git status | grep "On branch" | awk '{print $3}'`
+branch=$1
 #Take the first 6 characters of the current head commit
 commit_cur=`git log | head -n1 | awk '{print $2}' | cut -c 1-6`
 
 #Dictionary and binary names
-head_build="build_${commit_cur}"
+cur_build="build_${branch}"
 release_build="build_release"
-release_binary="iqtree_release"
+release_binary_prefix="iqtree_release"
 #cur_binary="iqtree_${commit_cur}"
-cur_binary="iqtree_test"
+cur_binary="iqtree_${branch}"
+bin_dir="iqtree_binaries"
 
-#Clean up 
-if [ -e $head_build ]
+#Clean up
+if [ -e $cur_build ]
 then
-  rm -rf $head_build
+  rm -rf $cur_build
 fi
 if [ -e $release_build ]
 then
   rm -rf $release_build 
 fi
-if [ -e $release_binary ]
+if [ -e $bin_dir ]
 then
-  rm -rf $release_binary
+  rm -rf $bin_dir
 fi
-if [ -e $cur_binary ]
+mkdir $bin_dir
+mkdir $cur_build 
+#Fetch changes from server
+git fetch
+curBranch=`git status | grep 'On branch' | awk '{print $3}'`
+if [ ${curBranch} != ${branch} ]
 then
-  rm -rf $cur_binary
+  git stash
+  git checkout $branch
+  git pull
+  git submodule update
 fi
-
-mkdir $head_build 
-cmake -B${head_build} -H..
-make -C ${head_build} -j4
-cp ${head_build}/iqtree ${cur_binary} 
-#rm -rf ${head_build}
+cmake -B${cur_build} -H..
+make -C ${cur_build} -j4
+cp ${cur_build}/iqtree ${bin_dir}/${cur_binary} 
+#rm -rf ${cur_build}
 mkdir $release_build
 #Find the hash code of the most recent release in master
 commit=`git log origin/master | grep -m 1 -B 4 "release version" | grep "commit" | awk '{print $2}'`
+version=`git log origin/master | grep -m 1 "release version [0-9]*" | awk '{print $3}'`
 git checkout ${commit}
 git submodule update
 cmake -B${release_build} -H..
 make -C ${release_build} -j4
-cp ${release_build}/iqtree ${release_binary}
+cp ${release_build}/iqtree ${bin_dir}/${release_binary_prefix}_${version}
 git checkout ${curBranch}
+git stash apply
 git submodule update
 
 #Clean up
-rm -rf $head_build
+rm -rf $cur_build
 rm -rf $release_build 
