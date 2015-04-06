29133acafbdad05004a1b34cb5c9bc298730d9a3
minh <minh.bui@univie.ac.at>
2015-04-06 19:42:31 +0200

    temporarily starting from BIONJ tree for partition model with non DNA and AA data

--- phyloanalysis.cpp
+++ phyloanalysis.cpp
@@ -1408,7 +1408,7 @@ void runTreeReconstruction(Params &params, string &original_model, IQTree &iqtre
 			PhyloSuperTree *stree = (PhyloSuperTree*)&iqtree;
 			for (PhyloSuperTree::iterator it = stree->begin(); it != stree->end(); it++)
 				if ((*it)->aln->seq_type != SEQ_DNA && (*it)->aln->seq_type != SEQ_PROTEIN)
-					params.start_tree = STT_PARSIMONY;
+					params.start_tree = STT_BIONJ;
 		} else if (iqtree.aln->seq_type != SEQ_DNA && iqtree.aln->seq_type != SEQ_PROTEIN)
 			params.start_tree = STT_PARSIMONY;
     }
