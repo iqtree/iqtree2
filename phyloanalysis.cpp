1c7e81294d7bd3519b0609a8fca2bdd74e6861df
minh <minh.bui@univie.ac.at>
2015-04-07 17:19:33 +0200

    set pattern log-likelihood to very small number when numerical underflow occurs

--- phyloanalysis.cpp
+++ phyloanalysis.cpp
@@ -56,7 +56,7 @@
 
 void reportReferences(Params &params, ofstream &out, string &original_model) {
 	out << "To cite IQ-TREE please use:" << endl << endl
-		<< "Lam-Tung Nguyen, Heiko A. Schmidt, Arndt von Haeseler, and Bui Quang Minh (2014)" << endl
+		<< "Lam-Tung Nguyen, Heiko A. Schmidt, Arndt von Haeseler, and Bui Quang Minh (2015)" << endl
 		<< "IQ-TREE: A fast and effective stochastic algorithm for estimating" << endl
 		<< "maximum likelihood phylogenies. Mol. Biol. Evol., 32:268-274." << endl << endl;
 
