a9aa2241aa43c5255f46f7f096ed1ecfb865b578
minh <minh.bui@univie.ac.at>
2015-05-13 16:15:39 +0200

    cleanup ModelCodon

--- model/modelcodon.h
+++ model/modelcodon.h
@@ -10,18 +10,6 @@
 
 #include "modelgtr.h"
 
-/**
- * parameter constraint
- */
-struct ParamConstraint {
-	bool fixed; // TRUE if this parameter is fixed
-	// minimum, initial, and maximum value
-	double min_value, init_value, max_value;
-	char opr, opr2; // operator: '*', '/', or 0, to force this parameter
-	int param1, param2, param3; // index of 2 parameters for operator
-	double opr_value, opr_value2; // instead of multiplying 2 parameters, one can multiply a parameter with this constant
-};
-
 /** CF_TARGET_NT: frequency of target nucleotide is multiplied with the rate entry (Muse and Gaut 1994)
     CF_TARGET_CODON: frequency of target codon is multiplied with the rate entry (Goldman Yang 1994)
     */
@@ -67,14 +55,15 @@ public:
 	*/
 	virtual int getNumRateEntries() { return num_states*(num_states); }
 
-	StateFreqType initCodon(const char *model_name, StateFreqType freq, bool reset_params);
-
 	/**
 		initialization, called automatically by the constructor, no need to call it
 		@param model_name model name, e.g., JC, HKY.
 		@param freq state frequency type
 	*/
 	virtual void init(const char *model_name, string model_params, StateFreqType freq, string freq_params);
+
+	StateFreqType initCodon(const char *model_name, StateFreqType freq, bool reset_params);
+
 
 	/**
 	 * @return model name with parameters in form of e.g. GTR{a,b,c,d,e,f}
@@ -88,74 +77,6 @@ public:
 		decompose the rate matrix into eigenvalues and eigenvectors
 	*/
 	virtual void decomposeRateMatrix();
-
-	/**
-	 * set rates into groups, rates within a group are equal
-	 * @param group assignment of each rate into group
-	 */
-//	void setRateGroup(IntVector &group);
-
-	/**
-	 * set rates into groups, rates within a group are equal
-	 * @param group assignment of each rate into group
-	 */
-//	void setRateGroup(const char *group);
-
-	/**
-	 * Set constraints for rate-groups, a comma-separated string of constraints.
-	 * Each constraint has the following format:
-	 *   xi=?       : rate of group x_i will be estimated from data
-	 *   xi=?value  : rate of group x_i will be initialized at value and then estimated from data
-	 *   xi=value   : rate of group x_i is fixed at a specific floating-point value
-	 *   xi>value   : rate of group x_i must be > value
-	 *   xi<value   : rate of group x_i must be < value
-	 *   xi=xj*xk   : rate of group x_i is constrained to equal to group x_j * group x_k
-	 *   xi=xj/xk   : rate of group x_i is constrained to equal to group x_j / group x_k
-	 *   @param constraint comma-separated string of constraints
-	 */
-//	void setRateGroupConstraint(string constraint);
-
-	/**
-		Read the rate parameters from a comma-separated string
-		It will throw error messages if failed
-		@param in input stream
-	*/
-//	virtual void readRates(string str) throw(const char*);
-
-	/**
-	 * @return true if codon1<->codon2 involves more than 1 nucleotide
-	 */
-//	bool isMultipleSubst(int state1, int state2);
-
-	/**
-	 * @return if single nucleotide substitution i (0<=i<=3) is involved from state1->state2, return
-	 * j*4+i, where j is the codon position of substitution. Otherwise return -1.
-	 */
-//	int targetNucleotide(int state1, int state2);
-
-	/**
-	 * @return true if codon1<->codon2 is a synonymous substitution
-	 */
-//	bool isSynonymous(int state1, int state2);
-
-	/**
-	 * @return true if codon1<->codon2 involves exactly one nucleotide transversion
-	 */
-//	bool isTransversion(int state1, int state2);
-
-    /**
-        count number of transition and transversion between 2 codons
-        @param state1 codon 1
-        @param state2 codon 2
-        @param ts (OUT) number of transitions (between 0 and 3)
-        @param tv (OUT) number of transversions (between 0 and 3)
-    */
-//    void countTsTv(int state1, int state2, int &ts, int &tv);
-
-	/** 3x4 matrix of nucleotide frequencies at 1st,2nd,3rd codon position */
-	double *ntfreq;
-
-
 
 	/**
 	 * read codon model from a stream, modying rates and state_freq accordingly
@@ -174,6 +95,19 @@ public:
 		@param out output stream
 	*/
 	virtual void writeInfo(ostream &out);
+
+    /** compute rate_attr for all codoni->codoni substitution */
+    void computeRateAttributes();
+    
+    /** combine rates with target nucleotide frequency (ntfreq) for MG-style model */
+    void combineRateNTFreq();
+
+    /** compute the corrected empirical omega (Kosiol et al 2007) */
+    double computeEmpiricalOmega();
+    
+
+	/** 3x4 matrix of nucleotide frequencies at 1st,2nd,3rd codon position */
+	double *ntfreq;
 
     /** dn/ds rate ratio */
     double omega;
@@ -202,19 +136,8 @@ public:
     /** rate atrributes */
     int *rate_attr;
     
-    /** compute rate_attr for all codoni->codoni substitution */
-    void computeRateAttributes();
-    
-//    bool combined_rate_ntfreq;
-    
-    /** combine rates with target nucleotide frequency (ntfreq) for MG-style model */
-    void combineRateNTFreq();
-
-    /** separate rates from target nucleotide frequency (ntfreq) for MG-style model */
-//    void separateRateNTFreq();
-
-    /** compute the corrected empirical omega (Kosiol et al 2007) */
-    double computeEmpiricalOmega();
+	/** empirical rates for empirical codon model or parametric+empirical codon model */
+	double *empirical_rates;
     
 protected:
 
@@ -236,16 +159,6 @@ protected:
         @return default frequency type
     */
 	StateFreqType initGY94(bool fix_kappa, CodonKappaStyle kappa_style);
-//
-//    /**
-//        @param kappa_ts true for Kappa(ts) model, false for Kappa(tv) (Kosiol et al 2007)
-//    */
-//	void initGY94_1K(bool kappa_ts);
-//
-//    /**
-//        Kappa(ts)Kappa(tv) model of Kosiol et al 2007
-//    */
-//	void initGY94_2K();
 
 	/**
 		this function is served for the multi-dimension optimization. It should pack the model parameters
@@ -261,17 +174,6 @@ protected:
 	*/
 	virtual void getVariables(double *variables);
 
-	/** assignment of each rate into group */
-//	IntVector rate_group;
-
-	/** constraint for each rate group */
-//	vector<ParamConstraint> rate_constraints;
-
-	/** empirical rates for empirical codon model or parametric+empirical codon model */
-	double *empirical_rates;
-
-	/** extra rate multiplier (e.g., frequency of target nucleotide for MG model) */
-//	double *extra_rates;
 };
 
 #endif /* MODELCODON_H_ */
