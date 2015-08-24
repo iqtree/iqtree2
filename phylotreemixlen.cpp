dbafb634855f5f74b9f71914769c044b02998321
minh <minh.bui@univie.ac.at>
2015-08-24 15:02:27 +0200

    start cpp

--- phylotreemixlen.cpp
+++ phylotreemixlen.cpp
@@ -7,3 +7,18 @@
 //
 
 #include "phylotreemixlen.h"
+
+PhyloTreeMixlen::PhyloTreeMixlen() : IQTree() {
+	mixlen = 1;
+}
+
+PhyloTreeMixlen::PhyloTreeMixlen(Alignment *aln, int mixlen) : IQTree(aln) {
+	cout << "Initializing heterotachy model with " << mixlen << " mixture branch lengths" << endl;
+	this->mixlen = mixlen;
+}
+
+void PhyloTreeMixlen::setMixlen(int mixlen) {
+
+}
+
+
