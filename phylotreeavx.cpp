089746a2f6025c0c82e457a7812e5f7a76429b61
Dominik Schrempf <dominik.schrempf@gmail.com>
2015-09-11 15:01:46 +0200

    BugFix; also modify setLikelihoodKernelAVX()

--- phylotreeavx.cpp
+++ phylotreeavx.cpp
@@ -101,6 +101,12 @@ void PhyloTree::setLikelihoodKernelAVX() {
 //	        cout << "Fast-AVX" << endl;
 		}
 		break;
+    case 16:// PoMo N=3
+			computeLikelihoodBranchPointer = &PhyloTree::computeLikelihoodBranchEigenSIMD<Vec4d, 4, 16>;
+			computeLikelihoodDervPointer = &PhyloTree::computeLikelihoodDervEigenSIMD<Vec4d, 4, 16>;
+			computePartialLikelihoodPointer = &PhyloTree::computePartialLikelihoodEigenSIMD<Vec4d, 4, 16>;
+			computeLikelihoodFromBufferPointer = &PhyloTree::computeLikelihoodFromBufferEigenSIMD<Vec4d, 4, 16>;
+            break;
     case 28:// PoMo N=5
 			computeLikelihoodBranchPointer = &PhyloTree::computeLikelihoodBranchEigenSIMD<Vec4d, 4, 28>;
 			computeLikelihoodDervPointer = &PhyloTree::computeLikelihoodDervEigenSIMD<Vec4d, 4, 28>;
