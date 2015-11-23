5a954db878cc6d0088fa954dcd1b6cc9d42c59cd
minh <minh.bui@univie.ac.at>
2015-11-23 14:36:36 +0100

    cleanup

--- phylolib/phylip_parser/ssort.h
+++
@@ -1,9 +0,0 @@
-#ifndef SSORT_H
-#define SSORT_H
-
-#define MIN(x,y) ((x) < (y) ? (x) : (y))
-#define SWAP(x,y) do{ __typeof__ (x) _t = x; x = y; y = _t; } while(0)
-
-int * ssort1main (char ** x, int n);
-
-#endif
