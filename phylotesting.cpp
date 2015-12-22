4bb41d83e88adb9bce55bb8a2532a0360d0567cc
minh <minh.bui@univie.ac.at>
2015-12-22 11:50:29 +0100

    linkTrees after model testing

--- phylotesting.cpp
+++ phylotesting.cpp
@@ -1143,6 +1143,7 @@ string testModel(Params &params, PhyloTree* in_tree, vector<ModelInfo> &model_in
 		// select model for each partition
 		PhyloSuperTree *stree = (PhyloSuperTree*)in_tree;
 		testPartitionModel(params, stree, model_info, fmodel, models_block);
+        stree->linkTrees();
 		string res_models = "";
 		for (vector<PartitionInfo>::iterator it = stree->part_info.begin(); it != stree->part_info.end(); it++) {
 			if (it != stree->part_info.begin()) res_models += ",";
