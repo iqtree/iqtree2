eb61bf59a8c8b5d05e0defacd5561e9a9a25efba
minh <minh.bui@univie.ac.at>
2016-08-23 22:33:12 +0200

    name invariant sites as constant or ambiguous constant sites

--- model/modelfactory.cpp
+++ model/modelfactory.cpp
@@ -358,14 +358,15 @@ ModelFactory::ModelFactory(Params &params, PhyloTree *tree, ModelsBlock *models_
 //		if (unobserved_ptns.size() <= 0)
 //			outError("Invalid use of +ASC because all constant patterns are observed in the alignment");
 		if (tree->aln->frac_invariant_sites > 0) {
+            cerr << tree->aln->frac_invariant_sites*tree->aln->getNSite() << " invariant sites are observed in the alignment (see below)" << endl;
             for (Alignment::iterator pit = tree->aln->begin(); pit != tree->aln->end(); pit++)
                 if (pit->isInvariant()) {
                     string pat_str = "";
                     for (Pattern::iterator it = pit->begin(); it != pit->end(); it++)
                         pat_str += tree->aln->convertStateBackStr(*it);
-                    outError(pat_str + " is invariable site", false);
+                    cerr << pat_str << " is invariant site pattern" << endl;
                 }
-			outError("Invalid use of +ASC because invariable sites are observed in the alignment");
+            outError("Invalid use of +ASC in the presence of invariant sites");
         }
 		cout << "Ascertainment bias correction: " << unobserved_ptns.size() << " unobservable constant patterns"<< endl;
 		rate_str = rate_str.substr(0, posasc) + rate_str.substr(posasc+4);
