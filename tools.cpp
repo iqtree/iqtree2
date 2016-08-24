844ee4e66549d3de0f7f9c4f68c964768b8fb46b
minh <minh.bui@univie.ac.at>
2016-08-24 10:26:03 +0200

    minor usage change

--- tools.cpp
+++ tools.cpp
@@ -3243,7 +3243,7 @@ void usage_iqtree(char* argv[], bool full_command) {
             << "                       MG1KTS, MG1KTV, MG2K" << endl
             << " Semi-empirical codon: XX_YY where XX is empirical and YY is mechanistic model" << endl
             << "       Morphology/SNP: MK (default), ORDERED" << endl
-            << "       Lie Markov DNA: Add prefix LM and a suffix RY, WS or MK to the following" << endl
+            << "       Lie Markov DNA: Add prefix LM and an optional suffix RY, WS or MK to:" << endl
             << "                       1.1,  2.2b, 3.3a, 3.3b,  3.3c," << endl
 	        << "                       3.4,  4.4a, 4.4b, 4.5a,  4.5b," << endl
 	        << "                       5.6a, 5.6b, 5.7a, 5.7b,  5.7c," << endl
@@ -3252,6 +3252,7 @@ void usage_iqtree(char* argv[], bool full_command) {
 	        << "                       6.17b,8.8,  8.10a,8.10b, 8.16," << endl
 	        << "                       8.17, 8.18, 9.20a,9.20b,10.12," << endl
 	        << "                       10.34,12.12" << endl
+            << "       Non-reversible: UNREST (most general unrestricted model)" << endl
             << "            Otherwise: Name of file containing user-model parameters" << endl
             << "                       (rate parameters and state frequencies)" << endl
             << endl << "STATE FREQUENCY:" << endl
