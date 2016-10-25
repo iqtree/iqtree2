3efd1fa6cd8ca25a5382022ab157735cd2a058c9
Bui Minh Quang <minh4@l31.cm.cluster>
2016-10-25 17:23:18 +0200

    fix intel compiler option

--- vectorclass/instrset_detect.cpp
+++ vectorclass/instrset_detect.cpp
@@ -20,11 +20,11 @@ namespace VCL_NAMESPACE {
 // input:  eax = functionnumber, ecx = 0
 // output: eax = output[0], ebx = output[1], ecx = output[2], edx = output[3]
 static inline void cpuid (int output[4], int functionnumber) {	
-#if defined (_MSC_VER) || defined (__INTEL_COMPILER)       // Microsoft or Intel compiler, intrin.h included
+#if defined (_MSC_VER) //|| defined (__INTEL_COMPILER)       // Microsoft or Intel compiler, intrin.h included
 
     __cpuidex(output, functionnumber, 0);                  // intrinsic function for CPUID
 
-#elif defined(__GNUC__) || defined(__clang__)              // use inline assembly, Gnu/AT&T syntax
+#elif defined(__GNUC__) || defined(__clang__) || defined (__INTEL_COMPILER)              // use inline assembly, Gnu/AT&T syntax
 
    int a, b, c, d;
    __asm("cpuid" : "=a"(a),"=b"(b),"=c"(c),"=d"(d) : "a"(functionnumber),"c"(0) : );
