c2c6b9c6f0af96a8ae45c8104efd01eec1d2fff7
minh <minh.bui@univie.ac.at>
2016-12-16 14:26:46 +0100

    disable EM for +R+ASC model which causes crash (reported by olaf.thalmann)

--- model/ratefree.cpp
+++ model/ratefree.cpp
@@ -210,7 +210,7 @@ double RateFree::optimizeParameters(double gradient_epsilon) {
 		cout << "Optimizing " << name << " model parameters by " << optimize_alg << " algorithm..." << endl;
 
     // TODO: turn off EM algorithm for +ASC model
-    if ((optimize_alg.find("EM") != string::npos && phylo_tree->getModelFactory()->unobserved_ptns.empty()) || getPInvar() <= MIN_PINVAR)
+    if ((optimize_alg.find("EM") != string::npos && phylo_tree->getModelFactory()->unobserved_ptns.empty()))
         return optimizeWithEM();
 
 	//if (freq_type == FREQ_ESTIMATE) scaleStateFreq(false);
