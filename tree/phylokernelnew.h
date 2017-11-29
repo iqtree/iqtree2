f56e464976dea05036c0bae2034d5f29083f92fc
Dominik Schrempf <dominik.schrempf@gmail.com>
2017-11-29 15:58:41 +0100

    Fix of isinf compilation error.

--- tree/phylokernelnew.h
+++ tree/phylokernelnew.h
@@ -2952,7 +2952,7 @@ double PhyloTree::computeLikelihoodBranchGenericSIMD(PhyloNeighbor *dad_branch, 
     if (std::isinf(tree_lh)) {
         tree_lh = 0.0;
         for (ptn = 0; ptn < orig_nptn; ptn++) {
-            if (isinf(_pattern_lh[ptn])) {
+          if (std::isinf(_pattern_lh[ptn])) {
                 _pattern_lh[ptn] = LOG_SCALING_THRESHOLD*4; // log(2^(-1024))
             }
             tree_lh += _pattern_lh[ptn] * ptn_freq[ptn];
