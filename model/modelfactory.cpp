2ed5782a2a40f1dad81142fa485a54cea946f5be
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-08-24 11:29:36 -0400

    Only increase precision for log-lh if VB_DEBUG or higher.

--- model/modelfactory.cpp
+++ model/modelfactory.cpp
@@ -1176,9 +1176,17 @@ double ModelFactory::optimizeParameters(int fixed_len, bool write_info,
     cur_lh = tree->computeLikelihood();
     tree->setCurScore(cur_lh);
     if (verbose_mode >= VB_MED || write_info) {
-	int p = cout.precision(17);
+	int p = -1;
+
+	// SET precision to 17 (temporarily)
+	if (verbose_mode >= VB_DEBUG) p = cout.precision(17);
+
+	// PRINT Log-Likelihood
 	cout << "1. Initial log-likelihood: " << cur_lh << endl;
-	cout.precision(p);
+
+	// RESTORE previous precision
+	if (verbose_mode >= VB_DEBUG) cout.precision(p);
+
         if (verbose_mode >= VB_MAX) {
             tree->printTree(cout);
             cout << endl;
