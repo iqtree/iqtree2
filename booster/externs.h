57c0792077b33a30c280eac53cd889f304ea2a10
Minh Bui <m.bui@anu.edu.au>
2018-09-07 17:41:51 +1000

    Add booster folder

---
+++ booster/externs.h
@@ -0,0 +1,25 @@
+/*
+
+BOOSTER: BOOtstrap Support by TransfER: 
+BOOSTER is an alternative method to compute bootstrap branch supports 
+in large trees. It uses transfer distance between bipartitions, instead
+of perfect match.
+
+Copyright (C) 2017 Frederic Lemoine, Jean-Baka Domelevo Entfellner, Olivier Gascuel
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
+extern int ntax; /* this is set in parse_nh, in tree.c */
