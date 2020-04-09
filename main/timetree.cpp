e839b11147b1c75838cb39ebd1b34ed2f7b877cd
Minh Bui <m.bui@anu.edu.au>
2020-04-09 10:14:30 +1000

    New option --date to input date file and accept YYYY-MM-DD date format, but better do this directly in LSD

--- main/timetree.cpp
+++ main/timetree.cpp
@@ -8,6 +8,82 @@
 #include "timetree.h"
 #include "lsd2/src/lsd.h"
 
+typedef vector<pair<string, string> > DateVector;
+#define YEAR_SCALE 100000
+
+/**
+ read a date file. Each line has two strings: name and date
+ */
+void readDateFile(string date_file, DateVector &dates) {
+    try {
+        ifstream in;
+        in.exceptions(ios::failbit | ios::badbit);
+        in.open(date_file);
+        int line_num;
+        for (line_num = 1; !in.eof(); line_num++) {
+            string line;
+            if (!safeGetline(in, line))
+                break;
+            if (line.empty()) // ignore empty line
+                continue;
+            string name, date;
+            istringstream line_in(line);
+            if (!(line_in >> name >> date))
+                throw "Line " + convertIntToString(line_num) + ": '" + line + "' does not contain name and date";
+            dates.push_back(std::make_pair(name, date));
+        }
+        in.clear();
+        // set the failbit again
+        in.exceptions(ios::failbit | ios::badbit);
+        in.close();
+    } catch (ios::failure) {
+        outError(ERR_READ_INPUT, date_file);
+    } catch (string str) {
+        outError(str);
+    } catch (...) {
+        outError("Error reading date file " + date_file);
+    }
+}
+
+
+/**
+ convert a date string in YYYY-MM-DD format into integer in unit of year*million
+ */
+string convertDateForLSD(string date_str) {
+    
+    StrVector str_vec;
+    convert_string_vec(date_str.c_str(), str_vec, '-');
+    if (str_vec.size() > 3)
+        outError("Invalid date string " + date_str + ": not in YYYY-MM-DD format");
+
+    int64_t year = convert_int64(str_vec[0].c_str());
+    int64_t month = 1;
+    int64_t day = 1;
+    if (str_vec.size() >= 2)
+        month = convert_int64(str_vec[1].c_str());
+    if (str_vec.size() == 3) {
+        day = convert_int64(str_vec[2].c_str());
+    }
+    if (month < 1 || month > 12)
+        outError("Invalid date string " + date_str + ": month is not between 1 and 12");
+    int64_t days_per_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int64_t days_of_year = 365;
+    if (year % 4 == 0) {
+        // leap year
+        days_per_month[1]++;
+        days_of_year++;
+    }
+    if (day < 1 || day > days_per_month[month-1])
+        outError("Invalid date string " + date_str + ": day is not between 1 and " +
+                 convertIntToString(days_per_month[month-1]));
+
+    int64_t elapsed_days = day-1;
+    for (int i = 0; i < month-1; i++)
+        elapsed_days += days_per_month[i];
+    return convertInt64ToString(year*YEAR_SCALE + elapsed_days*YEAR_SCALE/days_of_year);
+}
+
+
 #ifdef USE_LSD2
 void runLSD2(PhyloTree *tree) {
     cout << "Building time tree by least-square dating (LSD) with command:" << endl;
@@ -20,13 +96,17 @@ void runLSD2(PhyloTree *tree) {
         StrVector outgroup_names;
         convert_string_vec(Params::getInstance().root, outgroup_names);
         string outgroup_file = basename + ".outgroup";
-        ofstream out;
-        out.open(outgroup_file);
-        out << outgroup_names.size() << endl;
-        for (auto outgroup : outgroup_names) {
-            out << outgroup << endl;
+        try {
+            ofstream out;
+            out.open(outgroup_file);
+            out << outgroup_names.size() << endl;
+            for (auto outgroup : outgroup_names) {
+                out << outgroup << endl;
+            }
+            out.close();
+        } catch (...) {
+            outError("Error writing file " + outgroup_file);
         }
-        out.close();
         arg.push_back("-g");
         arg.push_back(outgroup_file);
         arg.push_back("-k");
@@ -36,6 +116,38 @@ void runLSD2(PhyloTree *tree) {
         arg.push_back("a");
     }
 
+    if (Params::getInstance().date_file != "") {
+        // parse the date file
+        DateVector dates;
+        cout << "Reading date file " << Params::getInstance().date_file << " ..." << endl;
+        readDateFile(Params::getInstance().date_file, dates);
+        // only retain taxon appearing in alignment
+        DateVector converted_dates;
+        cout << "ID\tTaxon\tDate\tConverted" << endl;
+        for (auto date: dates) {
+            int taxon_id = tree->aln->getSeqID(date.first);
+            if (taxon_id >= 0) {
+                string converted_date = convertDateForLSD(date.second);
+                converted_dates.push_back(std::make_pair(date.first, converted_date));
+                cout << taxon_id+1 << "\t" << date.first << "\t" << date.second << "\t" << converted_date << endl;
+            }
+        }
+        cout << converted_dates.size() << " dates extracted" << endl;
+        string date_file = basename + ".date";
+        try {
+            ofstream out;
+            out.open(date_file);
+            out << converted_dates.size() << endl;
+            for (auto date : converted_dates)
+                out << date.first << " " << date.second << endl;
+            out.close();
+        } catch (...) {
+            outError("Error writing file " + date_file);
+        }
+        arg.push_back("-d");
+        arg.push_back(date_file);
+    }
+    
     if (Params::getInstance().dating_options != "") {
         // extra options for LSD
         StrVector options;
