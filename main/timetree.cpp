15f99737b0c617caa2f7f6833a16a912ed762bc8
Minh Bui <m.bui@anu.edu.au>
2020-04-11 18:46:19 +1000

    internal interface with LSD2 instead of files

--- main/timetree.cpp
+++ main/timetree.cpp
@@ -46,69 +46,59 @@ void readDateFile(string date_file, DateVector &dates) {
 }
 
 
-/**
- convert a date string in YYYY-MM-DD format into integer in unit of year*million
- */
-string convertDateForLSD(string date_str) {
-    
-    StrVector str_vec;
-    convert_string_vec(date_str.c_str(), str_vec, '-');
-    if (str_vec.size() > 3)
-        outError("Invalid date string " + date_str + ": not in YYYY-MM-DD format");
-
-    int64_t year = convert_int64(str_vec[0].c_str());
-    int64_t month = 1;
-    int64_t day = 1;
-    if (str_vec.size() >= 2)
-        month = convert_int64(str_vec[1].c_str());
-    if (str_vec.size() == 3) {
-        day = convert_int64(str_vec[2].c_str());
+void writeOutgroup(ostream &out, const char *outgroup) {
+    StrVector outgroup_names;
+    convert_string_vec(outgroup, outgroup_names);
+    try {
+        out << outgroup_names.size() << endl;
+        for (auto outgroup : outgroup_names) {
+            out << outgroup << endl;
+        }
+    } catch (...) {
+        ASSERT(0 && "Error writing outgroup stream");
     }
-    if (month < 1 || month > 12)
-        outError("Invalid date string " + date_str + ": month is not between 1 and 12");
-    int64_t days_per_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int64_t days_of_year = 365;
-    if (year % 4 == 0) {
-        // leap year
-        days_per_month[1]++;
-        days_of_year++;
-    }
-    if (day < 1 || day > days_per_month[month-1])
-        outError("Invalid date string " + date_str + ": day is not between 1 and " +
-                 convertIntToString(days_per_month[month-1]));
-
-    int64_t elapsed_days = day-1;
-    for (int i = 0; i < month-1; i++)
-        elapsed_days += days_per_month[i];
-    return convertInt64ToString(year*YEAR_SCALE + elapsed_days*YEAR_SCALE/days_of_year);
 }
 
+void writeDate(ostream &out, PhyloTree *tree) {
+    DateVector dates;
+    cout << "Reading date file " << Params::getInstance().date_file << " ..." << endl;
+    readDateFile(Params::getInstance().date_file, dates);
+    // only retain taxon appearing in alignment
+    DateVector retained_dates;
+    cout << "ID\tTaxon\tDate" << endl;
+    for (auto date: dates) {
+        int taxon_id = tree->aln->getSeqID(date.first);
+        if (taxon_id >= 0) {
+            retained_dates.push_back(date);
+            cout << taxon_id+1 << "\t" << date.first << "\t" << date.second << endl;
+        }
+    }
+    cout << retained_dates.size() << " dates extracted" << endl;
+    try {
+        out << retained_dates.size() << endl;
+        for (auto date : retained_dates)
+            out << date.first << " " << date.second << endl;
+    } catch (...) {
+        ASSERT(0 && "Error writing date stream");
+    }
+}
 
 #ifdef USE_LSD2
 void runLSD2(PhyloTree *tree) {
     cout << "Building time tree by least-square dating (LSD) with command:" << endl;
+    
     string basename = (string)Params::getInstance().out_prefix + ".timetree";
     string treefile = basename + ".subst";
-    tree->printTree(treefile.c_str());
+    stringstream tree_stream, outgroup_stream, date_stream;
+    tree->printTree(tree_stream);
     StrVector arg = {"lsd", "-i", treefile, "-s", convertIntToString(tree->getAlnNSite()), "-c", "-o", basename};
+    
     if (Params::getInstance().root) {
         // print outgroup file for LSD
-        StrVector outgroup_names;
-        convert_string_vec(Params::getInstance().root, outgroup_names);
+        writeOutgroup(outgroup_stream, Params::getInstance().root);
         string outgroup_file = basename + ".outgroup";
-        try {
-            ofstream out;
-            out.open(outgroup_file);
-            out << outgroup_names.size() << endl;
-            for (auto outgroup : outgroup_names) {
-                out << outgroup << endl;
-            }
-            out.close();
-        } catch (...) {
-            outError("Error writing file " + outgroup_file);
-        }
         arg.push_back("-g");
-        arg.push_back(outgroup_file);
+        arg.push_back(outgroup_file); // only fake file
         arg.push_back("-k");
     } else {
         // search for all possible rootings
@@ -118,36 +108,14 @@ void runLSD2(PhyloTree *tree) {
 
     if (Params::getInstance().date_file != "") {
         // parse the date file
-        DateVector dates;
-        cout << "Reading date file " << Params::getInstance().date_file << " ..." << endl;
-        readDateFile(Params::getInstance().date_file, dates);
-        // only retain taxon appearing in alignment
-        DateVector converted_dates;
-        cout << "ID\tTaxon\tDate\tConverted" << endl;
-        for (auto date: dates) {
-            int taxon_id = tree->aln->getSeqID(date.first);
-            if (taxon_id >= 0) {
-                string converted_date = convertDateForLSD(date.second);
-                converted_dates.push_back(std::make_pair(date.first, converted_date));
-                cout << taxon_id+1 << "\t" << date.first << "\t" << date.second << "\t" << converted_date << endl;
-            }
-        }
-        cout << converted_dates.size() << " dates extracted" << endl;
+        writeDate(date_stream, tree);
         string date_file = basename + ".date";
-        try {
-            ofstream out;
-            out.open(date_file);
-            out << converted_dates.size() << endl;
-            for (auto date : converted_dates)
-                out << date.first << " " << date.second << endl;
-            out.close();
-        } catch (...) {
-            outError("Error writing file " + date_file);
-        }
         arg.push_back("-d");
-        arg.push_back(date_file);
+        arg.push_back(date_file);  // only fake file
     }
     
+    lsd::InputOutputStream io(tree_stream.str(), outgroup_stream.str(), date_stream.str());
+
     if (Params::getInstance().dating_options != "") {
         // extra options for LSD
         StrVector options;
@@ -163,12 +131,38 @@ void runLSD2(PhyloTree *tree) {
         argv[i] = (char*)arg[i].c_str();
     std::copy(arg.begin(), arg.end(), std::ostream_iterator<string>(std::cout, " "));
     cout << endl;
-    lsd_main(argc, argv);
+    
+    // main call to LSD!
+    lsd::buildTimeTree(argc, argv, &io);
+    
+    // fetch the output
+    string report_file = basename + ".lsd";
+    //string tree1_file = basename + ".raw";
+    string tree2_file = basename + ".nex";
+    string tree3_file = basename + ".nwk";
+    try {
+        ofstream out;
+        out.open(report_file);
+        out << ((ostringstream*)io.outResult)->str();
+        out.close();
+//        out.open(tree1_file);
+//        out << ((ostringstream*)io.outTree1)->str();
+//        out.close();
+        out.open(tree2_file);
+        out << ((ostringstream*)io.outTree2)->str();
+        out.close();
+        out.open(tree3_file);
+        out << ((stringstream*)io.outTree3)->str();
+        out.close();
+    } catch (...) {
+        outError("Couldn't write LSD output files");
+    }
+    
     cout << "LSD results written to:" << endl;
-    cout << "  LSD report:                  " << basename << endl;
-    cout << "  Time tree in nexus format:   " << basename << ".nexus" << endl;
-    cout << "  Time tree with dates:        " << basename << ".date.nexus" << endl;
-    cout << "  Time tree in newick format:  " << basename << ".nwk" << endl;
+    cout << "  LSD report:                  " << report_file << endl;
+//    cout << "  Time tree in nexus format:   " << tree1_file << endl;
+    cout << "  Time tree in nexus format:   " << tree2_file << endl;
+    cout << "  Time tree in newick format:  " << tree3_file << endl;
     cout << endl;
 }
 #endif
