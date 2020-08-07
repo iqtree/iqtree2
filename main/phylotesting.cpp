iqtree/iqtree2

commit e9f7061da0d059164afa3b050e7803e4c2cbcb5d

Extensive changes aimed at speeding up tree loading & parameter optimisation (~2x) & tree search (~2x) by "serialising" their memory access patterns to improve spatial and temporal locality of reference.
1. PhyloTree alignment summaries may now be "borrowed" from another tree
   that has the same alignment (the relevant member is isSummaryBorrowed;
   if it is true, this instance doesn't own the summary, it is only
   "Borrowing" a reference to a summary owned by another tree).
2. PhyloTree member functions copyPhyloTree and copyPhyloTreeMixlen take an
   extra parameter indicating whether the copy is to "borrow" a copy of the
   alignment summary of the original (if it has one).  This matters a lot
   for ratefree.cpp and +R free rate models, and modelmixture.cpp!
   The temporary copies of the phylo tree that are used during parameter
   Optimization can now re-use the AlignmentSummary of the original; which
   means they can "linearise" their memory access to sites, when they are
   Optimising branch lengths (see changes listed below, e.g. #4, #5, #6, #7).
3. PhyloTree::setAlignment does its "name check" a different way (rather
   than finding each sequence by name by scanning the tree, if asks
   MTree::getMapOfTaxonNameToNode for a name to leaf node map, and checks
   the array of sequence names against the map (updating the id on the
   node for each hit).  The new approach is equivalent but is much faster,
   O(n.ln(n)) rather than O(n^2).  This speeds up tree loads markedly
   (particularly for large trees), but it matters most for free rate
   parameter optimization (on middling inputs this was a significant
   factor: about ~10% of parameter optimization time).  This can be turned
   off by changing the FAST_NAME_CHECK symbol.
4. IQTree::optimizeModelParameters now calls prepareToComputeDistances()
   (So that AlignmentSummary's matrix of converted sequences) will be
   available (to be borrowed, via calls to PhyloTree::copyPhyloTree (see

tree/iqtree.cpp
@@ -2042,6 +2042,7 @@ double IQTree::perturb(int times) {
 extern pllUFBootData * pllUFBootDataPtr;
 
 string IQTree::optimizeModelParameters(bool printInfo, double logl_epsilon) {
+    prepareToComputeDistances();
     if (logl_epsilon == -1)
         logl_epsilon = params->modelEps;
     cout << "Estimate model parameters (epsilon = " << logl_epsilon << ")" << endl;
@@ -2074,7 +2075,6 @@ string IQTree::optimizeModelParameters(bool printInfo, double logl_epsilon) {
         } else {
             modOptScore = getModelFactory()->optimizeParameters(params->fixed_branch_length, printInfo, logl_epsilon);
         }
-
         if (isSuperTree()) {
             ((PhyloSuperTree*) this)->computeBranchLengths();
         }
@@ -2083,7 +2083,6 @@ string IQTree::optimizeModelParameters(bool printInfo, double logl_epsilon) {
             outWarning("Estimated model parameters are at boundary that can cause numerical instability!");
             cout << endl;
         }
-
         if (modOptScore < curScore - 1.0 && params->partition_type != TOPO_UNLINKED) {
             cout << "  BUG: Tree logl gets worse after model optimization!" << endl;
             cout << "  Old logl: " << curScore << " / " << "new logl: " << modOptScore << endl;
@@ -2096,7 +2095,7 @@ string IQTree::optimizeModelParameters(bool printInfo, double logl_epsilon) {
         if (params->print_trees_site_posterior)
             computePatternCategories();
     }
-
+    doneComputingDistances(); //DISABLED
     return newTree;
 }
 
@@ -2912,7 +2911,9 @@ pair<int, int> IQTree::doNNISearch(bool write_info) {
                 PLL_TRUE, 0, 0, 0, PLL_SUMMARIZE_LH, 0, 0);
         readTreeString(string(pllInst->tree_string));
     } else {
+        prepareToComputeDistances();
         nniInfos = optimizeNNI(Params::getInstance().speednni);
+        doneComputingDistances();
         if (isSuperTree()) {
             ((PhyloSuperTree*) this)->computeBranchLengths();
         }

commit 405fae1eaaf45e7beae085448ba0c89bf394da2a

Fix for fault introduced in commit e9f7061 (see change #5 below). Sometimes PhyloTree::getBestNNIForBran did a delete [] on a pointer it didn't own, resulting in segmentation faults (when called on Partitioned trees).  Now, it *never* has an nniMoves pointer that it allocates on the heap, so it never has to delete[] it (Instead it allocates a size-2 array of NNIMove on the stack).
1. PartInfo constructor now sets cur_ptnlh to nullptr.
2. NNIMove constructor now sets node1, node2, ptnlh to nullptr,
   And sets newloglh to -DBL_MAX.
3. Removed code in PhyloSuperTree::getBestNNIForBran that explicitly
   initialised those members of NNIMove (and moved declarations of
   Local NNIMove instances closer to the places they're used).
4. In PhyloSuperTreePlen::getBestNNIForBran, the local allocation
   of two NNIMove instances is done on the stack, so there's no need
   to keep track of whether it was used, and no need to delete [] it
   before function exit (if so).  There's no need to initialise the
   ptnlh members of those instances (or the node member of the first),
   Since the constructor does that (see change #2 above).  That's why
   There's no ... if (newNNIMoves) { delete [] nniMoves; } at the end.
5. In PhyloTree::getBestNNIForBran, likewise.  There's no
   delete [] nniMoves at the end (that should've been if done only
   if (newNNIMoves) was true; the defect introduced in commit e9f7061
   was that it was *no longer* checking if newNNIMoves was true).
6. PhyloTree::computeNNIPatternLh removed code explicitly setting
   node1 and node2 members (of two locally-declared NNIMove instances)
   to nullptr.  The constructor now does that (see change #2 above).
7. Various getBestNNIForBran declarations now have nniMoves defaulting
   to nullptr rather than NULL.
8. Code in main/phylotesting.cpp that was explicitly initialising
   PartInfo::cur_ptnlh and NNIMove::ptnlh is no longer needed
   (Because the constructors do that, see changes #1 and #2 above).

main/phylotesting.cpp
@@ -1442,9 +1442,8 @@ void mergePartitions(PhyloSuperTree* super_tree, vector<set<int> > &gene_sets, S
                 }
 			}
 		}
-		info.cur_ptnlh = NULL;
-		info.nniMoves[0].ptnlh = NULL;
-		info.nniMoves[1].ptnlh = NULL;
+        //Partinfo constructor now sets cur_tpnlh to nullptr (James B. 06-Aug-2020).
+        //NNIMove  constructor now sets ptnlh to nullptr     (James B. 06-Aug-2020).
 		part_info.push_back(info);
 		PhyloTree *tree = super_tree->extractSubtree(*it);
         tree->setParams(super_tree->params);

tree/iqtree.cpp
@@ -3419,7 +3419,7 @@ void IQTree::setDelete(int _delete) {
 
 void IQTree::evaluateNNIs(Branches &nniBranches, vector<NNIMove>  &positiveNNIs) {
     for (Branches::iterator it = nniBranches.begin(); it != nniBranches.end(); it++) {
-        NNIMove nni = getBestNNIForBran((PhyloNode*) it->second.first, (PhyloNode*) it->second.second, NULL);
+        NNIMove nni = getBestNNIForBran((PhyloNode*) it->second.first, (PhyloNode*) it->second.second, nullptr);
         if (nni.newloglh > curScore) {
             positiveNNIs.push_back(nni);
         }

tree/phylosupertree.cpp
@@ -845,8 +845,6 @@ NNIMove PhyloSuperTree::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NN
         node1 = node2;
         node2 = tmp;
     }
-    NNIMove myMove;
-    //myMove.newloglh = 0;
 	SuperNeighbor *nei1 = ((SuperNeighbor*)node1->findNeighbor(node2));
 	SuperNeighbor *nei2 = ((SuperNeighbor*)node2->findNeighbor(node1));
 	ASSERT(nei1 && nei2);
@@ -882,15 +880,15 @@ NNIMove PhyloSuperTree::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NN
         nni_ok[nniid++] = constraintTree.isCompatible(nni);
     }
     ASSERT(nniid == 2);
-    myMove.node1 = myMove.node2 = NULL;
-    myMove.newloglh = -DBL_MAX;
     // return if both NNIs do not satisfy constraint
     if (!nni_ok[0] && !nni_ok[1]) {
-//        ASSERT(!nniMoves);
         if (nniMoves) {
             nniMoves[0].newloglh = nniMoves[1].newloglh = -DBL_MAX;
         }
-        return myMove;
+        NNIMove nullMove;
+        //NNIMove constructor now sets node1 and node2 members
+        //to nullptr, and newloglh to -DBL_MAX (James B. 06-Aug-2020).
+        return nullMove;
     }
 
 	//double bestScore = optimizeOneBranch(node1, node2, false);
@@ -975,6 +973,9 @@ NNIMove PhyloSuperTree::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NN
     if (!nni_ok[0]) nni_scores[0] = -DBL_MAX;
     if (!nni_ok[1]) nni_scores[1] = -DBL_MAX;
 
+    NNIMove myMove;
+    //NNIMove constructor now sets node1 and node2 members
+    //to nullptr, and newloglh to -DBL_MAX (James B. 06-Aug-2020).
 	myMove.node1Nei_it = node1->findNeighborIt(node1_nei->node);
 	myMove.node1 = node1;
 	myMove.node2 = node2;
@@ -988,10 +989,11 @@ NNIMove PhyloSuperTree::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NN
 		myMove.newloglh = nni_scores[1];
 	}
 
-	if (save_all_trees != 2 && !nniMoves) return myMove;
-
+    if (save_all_trees != 2 && !nniMoves) {
+        return myMove;
+    }
 	// for bootstrap now
-    //now setup pattern likelihoods per partition
+    // now setup pattern likelihoods per partition
 	double *save_lh_factor = new double [ntrees];
 	double *save_lh_factor_back = new double [ntrees];
 	nniid = 0;
@@ -1041,9 +1043,7 @@ NNIMove PhyloSuperTree::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NN
         node2->updateNeighbor(node2_it, node2_nei);
         node2_nei->node->updateNeighbor(node1, node2);
         nniid++;
-
 	}
-
 	delete [] save_lh_factor_back;
 	delete [] save_lh_factor;
 	return myMove;

tree/phylosupertree.h
@@ -47,6 +47,9 @@ struct PartitionInfo {
     //double *nni1_ptnlh; // pattern likelihoods of 1st NNI tree
     //double *nni2_ptnlh; // pattern likelihoods of 2nd NNI tree
     NNIMove nniMoves[2];
+    PartitionInfo(): cur_ptnlh(nullptr) {        
+    }
+    
 };
 
 /**
@@ -303,7 +306,7 @@ public:
             @param node1 1 of the 2 nodes on the branch
             @param node2 1 of the 2 nodes on the branch
      */
-    virtual NNIMove getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove *nniMoves = NULL);
+    virtual NNIMove getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove *nniMoves = nullptr);
 
     /**
             Do an NNI on the supertree and synchronize all subtrees respectively

tree/phylosupertreeplen.cpp
@@ -417,17 +417,11 @@ NNIMove PhyloSuperTreePlen::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2
 
 	// ------------------------------------------------------------------
     int cnt;
-
-	//NNIMove nniMoves[2];
-    bool newNNIMoves = false;
-    if (!nniMoves) {
-		//   Initialize the 2 NNI moves
-    	newNNIMoves = true;
-    	nniMoves = new NNIMove[2];
-    	nniMoves[0].ptnlh = nniMoves[1].ptnlh = NULL;
-    	nniMoves[0].node1 = NULL;
+    NNIMove localNNIMoves[2];
+    if (nniMoves==nullptr) {
+        nniMoves = localNNIMoves;
+        //NNIMove constructor now sets node1 and ptnlh to nullptr (James B. 06-Aug-2020)
     }
-
     if (nniMoves[0].node1) {
     	// assuming that node1Nei_it and node2Nei_it are defined in nniMoves structure
     	for (cnt = 0; cnt < 2; cnt++) {
@@ -472,18 +466,15 @@ NNIMove PhyloSuperTreePlen::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2
 	 // restore curScore
 	 curScore = backupScore;
 
-	 NNIMove myMove;
-	 if (nniMoves[0].newloglh > nniMoves[1].newloglh) {
-		 myMove = nniMoves[0];
-		 myMove.swap_id = 1;
-	 } else {
-		 myMove = nniMoves[1];
-		 myMove.swap_id = 2;
-	 }
-	if (newNNIMoves) {
-		delete [] nniMoves;
-	}
-	return myMove;
+    NNIMove myMove;
+    if (nniMoves[0].newloglh > nniMoves[1].newloglh) {
+        myMove = nniMoves[0];
+        myMove.swap_id = 1;
+    } else {
+        myMove = nniMoves[1];
+        myMove.swap_id = 2;
+    }
+    return myMove;
 }
 
 void PhyloSuperTreePlen::doNNIs(vector<NNIMove> &compatibleNNIs, bool changeBran) {
@@ -536,9 +527,7 @@ void PhyloSuperTreePlen::doNNI(NNIMove &move, bool clearLH)
 	part_move.resize(ntrees);
 	getNNIType(move.node1, move.node2, is_nni);
 
-
 	for (it = begin(), part = 0; it != end(); it++, part++) {
-
 		if(is_nni[part] == NNI_NO_EPSILON){
 			PhyloNeighbor *nei1_part = nei1->link_neighbors[part];
 			PhyloNeighbor *nei2_part = nei2->link_neighbors[part];

tree/phylosupertreeplen.h
@@ -184,7 +184,7 @@ public:
             @param node1 1 of the 2 nodes on the branch
             @param node2 1 of the 2 nodes on the branch
      */
-    virtual NNIMove getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove *nniMoves = NULL);
+    virtual NNIMove getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove *nniMoves = nullptr);
 
 
     /**

tree/phylotree.cpp
@@ -3936,8 +3936,7 @@ NNIMove PhyloTree::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove
     /*
     if(params->upper_bound_NNI){
         totalNNIub += 2;
-        NNIMove resMove;
-        resMove = getBestNNIForBranUB(node1,node2,this);
+        NNIMove resMove = getBestNNIForBranUB(node1,node2,this);
         // if UB is smaller than the current likelihood, then we don't recompute the likelihood of the swapped topology.
         // Otherwise, follow the normal procedure: evaluate NNIs and compute the likelihood.
 
@@ -3988,26 +3987,21 @@ NNIMove PhyloTree::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove
         }
 //        ((PhyloNeighbor*) (*saved_it[id]))->scale_num = newScaleNum();
     }
-    if (params->nni5)
+    if (params->nni5) {
         ASSERT(mem_id == 2);
+    }
 
     // get the Neighbor again since it is replaced for saving purpose
     PhyloNeighbor* node12_it = (PhyloNeighbor*) node1->findNeighbor(node2);
     PhyloNeighbor* node21_it = (PhyloNeighbor*) node2->findNeighbor(node1);
 
     int cnt;
-
-    //NNIMove nniMoves[2];
-    bool newNNIMoves = false;
+    
+    NNIMove localNNIMoves[2];
     if (!nniMoves) {
-        //   Initialize the 2 NNI moves
-        newNNIMoves = true;
-        nniMoves = new NNIMove[2];
-        nniMoves[0].ptnlh = nniMoves[1].ptnlh = NULL;
-        nniMoves[0].node1 = NULL;
-
+        nniMoves = localNNIMoves;
+        //NNIMove constructor now sets node1, node2, and ptnlh to nullptr (James B. 06-Aug-2020)
     }
-
     if (nniMoves[0].node1) {
         // assuming that node1Nei_it and node2Nei_it is defined in nniMoves structure
         for (cnt = 0; cnt < 2; cnt++) {
@@ -4150,22 +4144,21 @@ NNIMove PhyloTree::getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove
 
     mem_slots.eraseSpecialNei();
 
-     // restore the length of 4 branches around node1, node2
-     FOR_NEIGHBOR(node1, node2, it)
-         (*it)->setLength((*it)->node->findNeighbor(node1));
-     FOR_NEIGHBOR(node2, node1, it)
-         (*it)->setLength((*it)->node->findNeighbor(node2));
+    // restore the length of 4 branches around node1, node2
+    FOR_NEIGHBOR(node1, node2, it)
+    (*it)->setLength((*it)->node->findNeighbor(node1));
+    FOR_NEIGHBOR(node2, node1, it)
+    (*it)->setLength((*it)->node->findNeighbor(node2));
+    
+    // restore curScore
+    curScore = backupScore;
 
-     // restore curScore
-     curScore = backupScore;
-
-     NNIMove res;
-     if (nniMoves[0].newloglh > nniMoves[1].newloglh) {
-         res = nniMoves[0];
-     } else {
-         res = nniMoves[1];
-     }
-    delete [] nniMoves;
+    NNIMove res;
+    if (nniMoves[0].newloglh > nniMoves[1].newloglh) {
+        res = nniMoves[0];
+    } else {
+        res = nniMoves[1];
+    }
     return res;
 }
 
@@ -4874,8 +4867,7 @@ void PhyloTree::computeNNIPatternLh(double cur_lh, double &lh2, double *pattern_
     nniMoves[1].ptnlh = pattern_lh3;
     bool nni5 = params->nni5;
     params->nni5 = true; // always optimize 5 branches for accurate SH-aLRT
-    nniMoves[0].node1 = nniMoves[1].node1 = NULL;
-    nniMoves[0].node2 = nniMoves[1].node2 = NULL;
+    //NNIMove constructor now sets node1 and node2 members to nullptr (James B. 06-Aug-2020)
     getBestNNIForBran(node1, node2, nniMoves);
     params->nni5 = nni5;
     lh2 = nniMoves[0].newloglh;

tree/phylotree.h
@@ -185,6 +185,9 @@ struct NNIMove {
     // pattern likelihoods
     double *ptnlh;
 
+    NNIMove(): node1(nullptr), node2(nullptr)
+        , newloglh(-DBL_MAX), ptnlh(nullptr) {
+    }
     bool operator<(const NNIMove & rhs) const {
         return newloglh > rhs.newloglh;
     }
@@ -1564,7 +1567,7 @@ public:
        @param node2 1 of the 2 nodes on the branch
        @param nniMoves (IN/OUT) detailed information of the 2 NNIs, set .ptnlh to compute pattern likelihoods
      */
-    virtual NNIMove getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove *nniMoves = NULL);
+    virtual NNIMove getBestNNIForBran(PhyloNode *node1, PhyloNode *node2, NNIMove *nniMoves = nullptr);
 
     /**
             Do an NNI