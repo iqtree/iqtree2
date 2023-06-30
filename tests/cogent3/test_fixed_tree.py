11ab3df68056883863abf7027cbe3bb07b2f8076
richard morris <richard@corkmate.com>
2023-06-30 12:54:36 +1000

    add iqtree1 test stub

---
+++ tests/cogent3/test_fixed_tree.py
@@ -0,0 +1,21 @@
+import subprocess
+import pathlib
+from numpy.testing import assert_allclose
+from .cogent3_utils import get_cogent3_result
+from tests.utils import iqtree2_log_liklihood, exec_command, run_test_using
+
+@pytest.mark.parametrize("data_files", [(["three-ungapped.fa"])], indirect=True)
+def test_iqtree_simple(data_files, temp_dir, repo_paths):
+    outpath = pathlib.Path("three-ungapped.fa.ckp.gz")
+    # need better determination of path
+    build_path = repo_paths["build_dir"]
+    cmnd =   build_path / "iqtree2 -s data/three-ungapped.fa -m HKY -redo"
+    _ = exec_command(cmnd)
+    lnL = iqtree2_log_liklihood(outpath)
+    # cogent3 value
+    c3_lf = get_cogent3_result()
+    # hope they're the same!
+    assert_allclose(lnL, c3_lf.lnL)
+
+if __name__ == "__main__":
+    run_test_using(__file__)
---
+++ tests/iqtree1/test_single_alignment.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+import pytest
+from tests.utils import run_test_using 
+
+@pytest.mark.parametrize("options", ['-cmin 2','-mtree'])
+@pytest.mark.parametrize(
+    "data_files", [(["example.phy"])], indirect=True
+)
+def test_single_alignment(temp_dir, data_files, repo_paths, options):
+    """
+    single alignment comparison between iqtree1 and iqtree2 
+
+    Parameters
+    ----------
+    options : list
+        Sample list of options from legacy testing framework's config.yaml
+    """
+
+    # Add your test logic here
+    assert True
+
+if __name__ == "__main__":
+    run_test_using(__file__)
