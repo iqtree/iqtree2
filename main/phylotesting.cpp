2f3fc2285ef7e1636060a7a587673091fa07e89d
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2024-08-06 10:43:07 +1000

    bug fix

--- main/phylotesting.cpp
+++ main/phylotesting.cpp
@@ -1492,23 +1492,23 @@ void runModelFinder(Params &params, IQTree &iqtree, ModelCheckpoint &model_info,
 
 
 #ifdef _CUDA
+    // mpi + cuda
+        if (num_threads== 1) {
+            double* nn_gpu_time_array = new double[num_processes];
+            double nn_gpu_time = NeuralNetwork::gpu_time;
+            // Gather static_var_value from all processes to the root process
+            MPI_Gather(&nn_gpu_time, 1, MPI_DOUBLE, nn_gpu_time_array, 1, MPI_DOUBLE, PROC_MASTER, MPI_COMM_WORLD);
 
-#if !defined(_OPENMP)      // mpi + cuda
-        double* nn_gpu_time_array = new double[num_processes];
-        double nn_gpu_time = NeuralNetwork::gpu_time;
-        // Gather static_var_value from all processes to the root process
-        MPI_Gather(&nn_gpu_time, 1, MPI_DOUBLE, nn_gpu_time_array, 1, MPI_DOUBLE, PROC_MASTER, MPI_COMM_WORLD);
+            cout << endl;
+            cout << num_processes << " processes are used for NN model selection with CUDA" << endl;
+            cout << "\tproc\twall_time\tcpu_time\tgpu_time" << endl;
 
-        cout << endl;
-        cout << num_processes << " processes are used for NN model selection with CUDA" << endl;
-        cout << "\tproc\twall_time\tcpu_time\tgpu_time" << endl;
-
-        if (MPIHelper::getInstance().isMaster()){
-            for (int p=0; p<num_processes; p++) {
-                cout << "\t" << p << "\t" << (nn_wall_time_array)[p] << "\t" << nn_cpu_time_array[p] << "\t" << nn_gpu_time_array[p] << endl;
+            if (MPIHelper::getInstance().isMaster()){
+                for (int p=0; p<num_processes; p++) {
+                    cout << "\t" << p << "\t" << (nn_wall_time_array)[p] << "\t" << nn_cpu_time_array[p] << "\t" << nn_gpu_time_array[p] << endl;
+                }
             }
         }
-#endif // openmp
 #else
         // no CUDA
         cout << num_processes << " processes are used for NN model selection" << endl;
