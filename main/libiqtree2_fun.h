cf47d3617480fd5f466ec6ad7aee2c7a4b9aecd6
Thomas Wong <thomaskf@gmail.com>
2024-09-05 15:47:16 +1000

    Add more descriptions about the PiQTREE functions

--- main/libiqtree2_fun.h
+++ main/libiqtree2_fun.h
@@ -17,14 +17,17 @@ int robinson_fould(const string& tree1, const string& tree2);
 
 // Generates a set of random phylogenetic trees
 // tree_gen_mode allows:"YULE_HARDING", "UNIFORM", "CATERPILLAR", "BALANCED", "BIRTH_DEATH", "STAR_TREE"
+// output: a newick tree (in string format)
 string random_tree(int num_taxa, string tree_gen_mode, int num_trees, int rand_seed = 0);
 
-// Perform phylogenetic analysis on the input alignment (in string format)
+// Perform phylogenetic analysis on the input alignment
 // With estimation of the best topology
+// output: results in YAML format with the tree and the details of parameters
 string build_tree(vector<string>& names, vector<string>& seqs, string model, int rand_seed = 0);
 
-// Perform phylogenetic analysis on the input alignment (in string format)
+// Perform phylogenetic analysis on the input alignment
 // With restriction to the input toplogy
+// output: results in YAML format with the details of parameters
 string fit_tree(vector<string>& names, vector<string>& seqs, string model, string intree, int rand_seed = 0);
 
 // Perform phylogenetic analysis with ModelFinder
@@ -33,6 +36,7 @@ string fit_tree(vector<string>& names, vector<string>& seqs, string model, strin
 // freq_set -- a set of frequency types
 // rate_set -- a set of RHAS models
 // rand_seed -- random seed, if 0, then will generate a new random seed
+// output: modelfinder results in YAML format
 string modelfinder(vector<string>& names, vector<string>& seqs, int rand_seed = 0,
                    string model_set = "", string freq_set = "", string rate_set = "");
 
