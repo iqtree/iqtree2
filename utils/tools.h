ba59930c3edb81d69e82e37285f6c7da4d3864b1
Thomas Wong <thomaskf@gmail.com>
2024-09-27 11:41:28 +1000

    Fixed the compilation issue under Windows

--- utils/tools.h
+++ utils/tools.h
@@ -378,9 +378,12 @@ const int WT_BR_ID = 512;
 const int WT_BR_LEN_ROUNDING = 1024;
 const int WT_BR_LEN_SHORT = 2048; // store only 6 digits after the comma for branch lengths
 const int WT_BR_ATTR = 4096; // print branch attributes
+const double ONE_THIRD = 1.0 / 3.0;
+
+#if ! (defined WIN32 || defined _WIN32 || defined __WIN32__ || defined WIN64)
 const int TRUE = 1;
 const int FALSE = 0;
-const double ONE_THIRD = 1.0 / 3.0;
+#endif
 
 /**
  *  Specify different ways of doing an NNI.
