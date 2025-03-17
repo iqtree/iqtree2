1320c4cc360bbacacc592911459aaa06cd4a7961
Thomas Wong <thomaskf@gmail.com>
2025-03-17 16:34:47 +1100

    Update for making iqtree lib files under Windows

---
+++ test_scripts/bridge.cpp
@@ -0,0 +1,72 @@
+#include<vector>
+#include<string>
+#include<iostream>
+
+/*
+ * Calculates the robinson fould distance between two trees
+ */
+int robinson_fould(const std::string& tree1, const std::string& tree2);
+
+/*
+ * Generates a set of random phylogenetic trees
+ * tree_gen_mode allows:"YULE_HARDING", "UNIFORM", "CATERPILLAR", "BALANCED", "BIRTH_DEATH", "STAR_TREE"
+ * output: a newick tree (in string format)
+ */
+std::string random_tree(int num_taxa, std::string tree_gen_mode, int num_trees, int rand_seed = 0);
+
+/*
+ * Perform phylogenetic analysis on the input alignment
+ * With estimation of the best topology
+ * num_thres -- number of cpu threads to be used, default: 1; 0 - auto detection of the optimal number of cpu threads
+ * output: results in YAML format with the tree and the details of parameters
+ */
+std::string build_tree(std::vector<std::string>& names, std::vector<std::string>& seqs, std::string model, int rand_seed = 0, int bootstrap_rep = 0, int num_thres = 1);
+
+/*
+ * Perform phylogenetic analysis on the input alignment
+ * With restriction to the input toplogy
+ * num_thres -- number of cpu threads to be used, default: 1; 0 - auto detection of the optimal number of cpu threads
+ * output: results in YAML format with the details of parameters
+ */
+std::string fit_tree(std::vector<std::string>& names, std::vector<std::string>& seqs, std::string model, std::string intree, int rand_seed = 0, int num_thres = 1);
+
+/*
+ * Perform phylogenetic analysis with ModelFinder
+ * on the input alignment (in string format)
+ * model_set -- a set of models to consider
+ * freq_set -- a set of frequency types
+ * rate_set -- a set of RHAS models
+ * rand_seed -- random seed, if 0, then will generate a new random seed
+ * num_thres -- number of cpu threads to be used, default: 1; 0 - auto detection of the optimal number of cpu threads
+ * output: modelfinder results in YAML format
+ */
+std::string modelfinder(std::vector<std::string>& names, std::vector<std::string>& seqs, int rand_seed = 0,
+                   std::string model_set = "", std::string freq_set = "", std::string rate_set = "", int num_thres = 1);
+
+/*
+ * Build pairwise JC distance matrix
+ * output: set of distances
+ * (n * i + j)-th element of the list represents the distance between i-th and j-th sequence,
+ * where n is the number of sequences
+ * num_thres -- number of cpu threads to be used, default: 1; 0 - use all available cpu threads on the machine
+ */
+std::vector<double> build_distmatrix(std::vector<std::string>& names, std::vector<std::string>& seqs, int num_thres = 1);
+
+/*
+ * Using Rapid-NJ to build tree from a distance matrix
+ * output: a newick tree (in string format)
+ */
+std::string build_njtree(std::vector<std::string>& names, std::vector<double>& distances);
+
+/*
+ * verion number
+ */
+std::string version();
+
+int main() {
+  std::string tree1 = "(a,b,(c,(d,e)));";
+  std::string tree2 = "(e,b,(c,(d,a)));";
+
+  std::cout << robinson_fould(tree1, tree2) << std::endl;
+  return 0;
+}
--- test_scripts/make-dll-lib.bat
+++ test_scripts/make-dll-lib.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 :: Build the DLL file and the DEF file
-g++ -shared -o libiqtree2.dll %1 -Wl,--output-def,libiqtree2.def -liqtree2 -L. -lws2_32 -lz -lpthread
+g++ -shared -o iqtree2.dll %1 -Wl,--output-def,iqtree2.def -liqtree2 -L. -lws2_32 -lz -lpthread
 
 :: Create the LIB file
-lib /def:libiqtree2.def
+lib /def:iqtree2.def
